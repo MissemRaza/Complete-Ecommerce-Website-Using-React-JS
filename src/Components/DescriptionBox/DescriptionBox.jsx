c75b80249d49563a6769781341326900610b345a
Missem Raza <ceomissem@gmail.com>
2024-02-12 14:28:46 +0500

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>  
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>E-commerce first made waves in Pakistan’s e-commerce market after its introduction in 2012. 
+                We have since grown to become Pakistan’s largest platform for online shopping with a
+                network spread across Asia in Pakistan, Bangladesh, Sri Lanka, Myanmar. 
+                Our vision was to provide a safe, efficient online marketplace platform for vendors and customers across the country to come together.
+            </p>
+            <p>
+                New Stylish Drawf Print Kurti Comfortable Linen Stuff Ready to wear Stylish New Print 
+                Casual & Party use  Bright color
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
